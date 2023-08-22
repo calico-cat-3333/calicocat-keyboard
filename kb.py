525aacdd5caf860bcb039723596927fed5f64e43
Calico cat <ahsdhmzyn@163.com>
2023-08-22 10:17:12 +0800

    fix

--- kb.py
+++ kb.py
@@ -3,34 +3,35 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners import DiodeOrientation
 from kmk.scanners.encoder import RotaryioEncoder
-from kmk.scanners.keypad import MatrixScanner
+from kmk.scanners.digitalio import MatrixScanner
 
 
 class KMKKeyboard(_KMKKeyboard):
-    self.extensions = []
+    extensions = []
 
-    self.matrix = [
-        MatrixScanner(
-            column_pins=self.col_pins,
-            row_pins=self.row_pins,
-            columns_to_anodes=DiodeOrientation.COL2ROW
-            ),
-        RotaryioEncoder(
-            pin_a=board.GP1,
-            pin_b=board.GP0
-            ),
-    ]
+    row_pins = [board.GP27, board.GP26, board.GP22, board.GP21, board.GP20, board.GP19, board.GP18, board.GP17, board.GP16]
+    col_pins = [board.GP15, board.GP14, board.GP13, board.GP12, board.GP11, board.GP10, board.GP9, board.GP8, board.GP7, board.GP6]
+    diode_orientation = DiodeOrientation.ROW2COL
 
-	self.row_pins = [board.GP27, board.GP26, board.GP22, board.GP21, board.GP20, board.GP19, board.GP18, board.GP17, board.GP16]
-    self.col_pins = [board.GP15, board.GP14, board.GP13, board.GP12, board.GP11, board.GP10, board.GP9, board.GP8, board.GP7, board.GP6]
-    self.diode_orientation = DiodeOrientation.ROW2COL
-
-    self.coord_mapping = [
+    coord_mapping = [
          0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 60, 61, 62, 63, 64, 65,
         10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 66, 67, 68, 69, 79,
         20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 70, 71, 72, 73, 74,
-        31, 32, 33, 34, 35, 36, 37, 38, 39, 75, 76, 77, 78,
-        41, 42, 43, 44, 45, 46, 47, 48, 49, 80, 81, 82, 83,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 75, 76, 77, 78,
+        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 80, 81, 82, 83,
         50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 84, 85,
-        86, 91, 92  # this three key for rotaryioencoder
+        86, 90, 91  # this three key for rotaryioencoder
         ]
+
+    def __init__(self):
+        self.matrix = [
+            MatrixScanner(
+                cols=self.col_pins,
+                rows=self.row_pins,
+                diode_orientation=self.diode_orientation,
+                ),
+            RotaryioEncoder(
+                pin_a=board.GP1,
+                pin_b=board.GP0
+                ),
+            ]
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.layers import Layers
 from kmk.extensions.RGB import RGB
+from kmk.extensions.rgb import AnimationModes
 
 
 keyboard = KMKKeyboard()
-rgb = RGB(pixel_pin=board.GP28, num_pixels=86, val_limit = 25)
+rgb = RGB(pixel_pin=board.GP28, num_pixels=86, val_limit = 10, animation_mode=AnimationModes.RAINBOW)
 
 keyboard.modules.append(Layers())
 keyboard.extensions.append(rgb)
@@ -17,12 +18,13 @@ keyboard.extensions.append(rgb)
 keyboard.keymap = [[
     KC.ESC,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.PSCR, KC.INS,  KC.DEL,
     KC.GRV,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS, KC.EQL,  KC.BSPC, KC.HOME,
-    KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC, KC.RBRC, KC.BSLS, KC.PGUP,
+    KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC, KC.RBRC, KC.BSLS, KC.END,
     KC.CAPS, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT, KC.ENT,  KC.PGUP,
     KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.RSFT, KC.UP,   KC.PGDN,
     KC.LCTL, KC.KP_1, KC.LGUI, KC.LALT, KC.SPC,  KC.SPC,  KC.RALT, KC.APP,  KC.RCTL, KC.LEFT, KC.DOWN, KC.RGHT,
-    KC.KB_2, KC.KP_3, KC.KB_4,
+    KC.KP_2, KC.KP_3, KC.KP_4,
         ]]
 
 if __name__ == '__main__':
+    keyboard.debug_enabled = True
     keyboard.go()
