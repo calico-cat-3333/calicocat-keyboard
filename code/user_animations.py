bd1b75584fa0677201327dd0e0f949ff81ec4b3f
Calico cat <ahsdhmzyn@163.com>
2023-11-14 23:27:31 +0800

    重命名部分变量

--- code/user_animations.py
+++ code/user_animations.py
@@ -5,7 +5,7 @@ from kmk.handlers.stock import passthrough as handler_passthrough
 import supervisor
 
 # 将电路上的rgb排列转换为更常见的排列
-rgb_index = [
+rgb_led_map = [
     [15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
     [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
     [45,44,43,42,41,40,39,38,37,36,35,34,33,32,31],
@@ -23,26 +23,26 @@ def stream(rgb):
 
 # 纵向彩色流光灯效  vertical stream animation
 def vertical_stream(rgb):
-    for j in range(0,len(rgb_index)):
-        for i in range(0, len(rgb_index[j])):
+    for j in range(0,len(rgb_led_map)):
+        for i in range(0, len(rgb_led_map[j])):
             rgb.set_hsv(
                 rgb.hue + j * rgb.hue_step,
                 rgb.sat,
                 rgb.val,
-                rgb_index[j][i]
+                rgb_led_map[j][i]
             )
     rgb.increase_hue(rgb._step)
 
 # 横向彩色流光灯效  horizontal stream animation
 # 推荐 hue_step: 20  recommended hue_step: 20
 def horizontal_stream(rgb):
-    for j in range(0,len(rgb_index)):
-        for i in range(0, len(rgb_index[j])):
+    for j in range(0,len(rgb_led_map)):
+        for i in range(0, len(rgb_led_map[j])):
             rgb.set_hsv(
                 rgb.hue + i * rgb.hue_step,
                 rgb.sat,
                 rgb.val,
-                rgb_index[j][i]
+                rgb_led_map[j][i]
             )
     rgb.increase_hue(rgb._step)
 
@@ -59,34 +59,34 @@ def init_rgb_modes_cycle_key(rgb):
         AnimationModes.KNIGHT,
         AnimationModes.SWIRL
     ]
-    user_animation_list = [
+    user_animations_list = [
         stream,
         vertical_stream,
         horizontal_stream
     ]
-    if not user_animation_list == []:
+    if not user_animations_list == []:
         rgb_modes_list.append(AnimationModes.USER)
         if rgb.user_animation == None:
-            rgb.user_animation = user_animation_list[0]
+            rgb.user_animation = user_animations_list[0]
     rgb_modes_num = len(rgb_modes_list)
-    user_animation_num = len(user_animation_list)
+    user_animations_num = len(user_animations_list)
 
     def rgb_modes_cycle(*args, **kwargs):
         rgb.effect_init = True
-        rgb_mode_index = 0
+        current_rgb_mode = 0
         if rgb.animation_mode == AnimationModes.STATIC_STANDBY:
-            rgb_mode_index = rgb_modes_list.index(AnimationModes.STATIC)
+            current_rgb_mode = rgb_modes_list.index(AnimationModes.STATIC)
         else:
-            rgb_mode_index = rgb_modes_list.index(rgb.animation_mode)
+            current_rgb_mode = rgb_modes_list.index(rgb.animation_mode)
 
         if rgb.animation_mode == AnimationModes.USER:
-            user_animation_index = (user_animation_list.index(rgb.user_animation) + 1) % user_animation_num
-            rgb.user_animation = user_animation_list[user_animation_index]
-            if not user_animation_index == 0:
-                rgb_mode_index = rgb_mode_index - 1
+            current_user_animation = (user_animations_list.index(rgb.user_animation) + 1) % user_animations_num
+            rgb.user_animation = user_animations_list[current_user_animation]
+            if not current_user_animation == 0:
+                current_rgb_mode = current_rgb_mode - 1
 
-        rgb_mode_index = (rgb_mode_index + 1) % rgb_modes_num
-        rgb.animation_mode = rgb_modes_list[rgb_mode_index]
+        current_rgb_mode = (current_rgb_mode + 1) % rgb_modes_num
+        rgb.animation_mode = rgb_modes_list[current_rgb_mode]
 
     make_key(
         names=('RGB_MODES_CYCLE', 'RGB_CYC'),
